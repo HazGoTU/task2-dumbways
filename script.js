127803f23b4057aed92ea1fbff78abafcdd3fc67
HazGoTU <haziqhabarkal@gmail.com>
2025-09-26 03:47:48 +0700

    update task3( tambahan  file content.html, modifikasi css,dan penggunaan bootsrap untuk styling halaman content,dan tambahan script di javascript untuk event button contact.)

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ const menuSelection = document.getElementById('selectionMenu')
 function showMenu(){
    if(menuSelection.style.display === "none"){
         menuSelection.style.display = "flex"
-        menuSelection.style.zIndex = "1"
-   
+        menuSelection.style.zIndex = "1"   
    }else{
         menuSelection.style.display = "none"
    }
    console.log("show menu")
    
+   
+}
+
+const windowContact = document.getElementById('windowContact').addEventListener('click', newWindow)
+function newWindow(){
+    window.open("/contact.html")
+    console.log("new window is openned")
 }
